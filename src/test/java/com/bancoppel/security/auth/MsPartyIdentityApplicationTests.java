9f9a9f554cc180ebfc6572a86c6659d39908213b
jgarcia-sps <jgarcia@spsolutions.com.mx>
2025-12-23 14:31:42 -0600

    version inicial

---
+++ src/test/java/com/bancoppel/security/auth/MsPartyIdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancoppel.security.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPartyIdentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
